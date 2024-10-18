a25ccd54ef7d9487b9e846ce4d6c689fcd15b0d3
luidimso <luidi_mso@outlook.com>
2024-10-18 17:26:40 -0300

    Card list component style

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import '../styles.css';
+import { GoPencil } from "@react-icons/all-files/go/GoPencil";
 
 const CardList = ({ cards, setCurrentCard }) => (
   <ul className="card-list">
     {cards.map(card => (
       <li key={card.id} className="card-item">
-        <h2>{card.name}</h2>
-        <p>**** **** **** {card.number.slice(-4)}</p>
-        <button onClick={() => setCurrentCard(card)}>Edit</button>
+        <div className='card-header'>
+          <img src="/mastercard-logo.svg" alt="image" style={{marginTop:"-15px"}}/>
+          <div className='card-infos'>
+            <div className='card-info'>
+              <p className='card-label'>CVC</p>
+              <p className='card-value'>{card.cvc}</p>
+            </div>
+            <div className='card-info'>
+              <p className='card-label'>EXPIRES</p>
+              <p className='card-value'>{card.expiry}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='card-footer'>
+          <p className='card-name'>{card.name}</p>
+          <div className='card-footer-bottom'>
+            <p className='card-number'>**** **** **** {card.number.slice(-4)}</p>
+            <GoPencil onClick={() => setCurrentCard(card)} className='icon'/>
+          </div>
+        </div>
+        {/* <button >Edit</button> */}
       </li>
     ))}
   </ul>
